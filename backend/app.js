86898e6d48eeea5e578860b2ce82a71415d288e7
Davaious <davidalcalde.hyland@gmail.com>
2024-12-20 19:30:13 +0100

    creacion de backend/app.js y su codigo inicial

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+// backend/app.js
+const express = require('express');
+const dotenv = require('dotenv');
+const validatorRoutes = require('./routes/validatorRoutes');
+const tokenRoutes = require('./routes/tokenRoutes');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use('/api', validatorRoutes);
+app.use('/api', tokenRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
+});
